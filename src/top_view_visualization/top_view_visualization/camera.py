896509aa2f8a9c3edc4ab5830068a01444b1a53b
Austin Tsai <aytsai@usc.edu>
2024-11-18 11:23:17 -0800

    added subscriber to robot position to display onto visualization image, may need adjustment to get correct layout

--- src/top_view_visualization/top_view_visualization/camera.py
+++ src/top_view_visualization/top_view_visualization/camera.py
@@ -1,23 +1,51 @@
 import rclpy
 from rclpy.node import Node 
 from sensor_msgs.msg import Image 
+from geoemtry_msgs.msg import Point
 from cv_bridge import CvBridge 
 import cv2 as cv
 from top_view_visualization.GoProInterface.webcam import GoProWebcamPlayer
 from top_view_visualization.camera_calibration.opencv_calibration.camera_distortion import CameraDistortion
 
-
 class CameraPublisher(Node):
     def __init__(self):
-        super().__init__('camera_publisher')
-        self.publisher_ = self.create_publisher(Image, 'scenario_image', 10)
-        timer_period = 0.1  # seconds
+        #Start GoPro Stream
         self.bridge = CvBridge()
         self.webcam = GoProStream()
         self.cameraDistortion = CameraDistortion()
         self.webcam.start_stream() #starts the http stream of photos
+        
+        #Create subscription to robot location
+        self.subscription = self.create_subscription(
+            Point,
+            'robot_location',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+        self.robot_location = (0, 0, 0)
+        
+        #Create publication of images
+        super().__init__('camera_publisher')
+        self.publisher_ = self.create_publisher(Image, 'scenario_image', 10)
+        timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback) #publish on the topic "scienario_image" every 0.1 seconds 
+    
+    def add_robot_position_text(self, img):
+        font                   = cv.FONT_HERSHEY_SIMPLEX
+        bottomLeftCornerOfText = (10,500)
+        fontScale              = 0.5
+        fontColor              = (255,255,255)
+        thickness              = 1
+        lineType               = 2
 
+        cv.putText(img, f'x:{self.robot_location[0]}, y:{self.robot_location[1]}, z:{self.robot_location[2]}', 
+            bottomLeftCornerOfText, 
+            font, 
+            fontScale,
+            fontColor,
+            thickness,
+            lineType)
+        
 
     def timer_callback(self):
         ret, frame = self.webcam.image_capture() #acts as openCV's cap.read() which returns a boolean and numpy array represetning the image
@@ -25,10 +53,13 @@ class CameraPublisher(Node):
         # ret = True #testing purposes 
         if ret:
             frame = self.cameraDistortion.undistort(frame)
+            self.add_robot_position_text(frame)
             self.publisher_.publish(self.bridge.cv2_to_imgmsg(frame))
             # self.get_logger().info('Publishing video frame')
-
-
+    
+    def listener_callback(self, msg):
+        self.robot_location = (msg.x, msg.y, msg.z)
+        
 def main(args=None):
     rclpy.init(args=args)
     camera_publisher = CameraPublisher()
